ee7ca142e0657366f282a6d33e76b53dc089577e
NicholasAsp <Nicholas.asper@gmail.com>
2021-11-03 16:37:27 -0500

    Adding the NJOY File while working on the python script to read the file

--- Helloworld.py
+++ Helloworld.py
@@ -1,1 +1,23 @@
-print("Goodbye, World!")
+import numpy as np
+lines = []
+with open('GA71TEndfix') as f:
+    lines = f.readlines()
+
+count = 0
+for line in lines:
+        count+=1
+        if "mt444" in line:
+            print(line)
+            break
+   
+for i in range (count+3, count+773):
+   linespl = lines[i].split(" ")
+   doublsplit = np.array(linespl)
+   fltsplit= doublsplit.astype(np,float)
+   print(fltsplit)
+
+    #'GA71TEndfix'
+
+            
+
+        
